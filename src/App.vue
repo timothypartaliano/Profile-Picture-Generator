1789c8815799550b37f8f1d2da2d62ec96169f2c
timothypartaliano <timothypartaliano@gmail.com>
2023-06-15 13:21:49 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="avatar-generator">
+    <input type="text" placeholder="Ketikkan nama anda.." class="input"><br>
+    <button @click="generateAvatar">Buat profile picture</button><br>
+    <img :src="avatarUrl" alt="Avatar" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      avatarUrl: ""
+    };
+  },
+  methods: {
+    generateAvatar() {
+      const hash = Math.random().toString(36).substring(7);
+      this.avatarUrl = `https://robohash.org/${hash}`;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.avatar-generator {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+button, input, img {
+  
+  padding: 10px 20px;
+  font-size: 16px;
 }
 </style>
